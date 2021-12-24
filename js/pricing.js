aef1c5615619e98766ca8d727f6c6db785a8678c
NataliaBodnar <bodnarnatalka@gmail.com>
2021-12-24 11:02:40 +0000

    added function with slides

--- js/pricing.js
+++ js/pricing.js
@@ -1,24 +1,20 @@
 (function() {
     const slides = [
-        '<div><img src="img/baby-yoda.svg" alt="Baby Yoda"></div>',
-        '<div><img src="img/banana.svg" alt="Banana"></div>',
-        '<div><img src="img/girl.svg" alt="Girl"></div>',
-        '<div><img src="img/viking.svg" alt="Viking"></div>'
+        '<div><img src="img/briofillyum.svg" alt="Briofillyum"></div>',
+        '<div><img src="img/cedar-pine.svg" alt="Cedar Pine"></div>',
+        '<div><img src="img/crassula-ovata.svg" alt="Crassula Ovata"></div>',
+        '<div><img src="img/elephant-bush.svg" alt="Elephant Bush"></div>'
+        '<div><img src="img/graptosedum.svg" alt="Graptosedum"></div>'
+        '<div><img src="img/money-plant.svg" alt="Money Plant"></div>'
+        '<div><img src="img/philodendron.svg" alt="Philodendron"></div>'
+        '<div><img src="img/raphis-palm.svg" alt="Raphis Palm"></div>'
     ];
 
     let currentSlide = 0;
 
     function showCurrentSlide() {
-        const slideContainer = document.querySelector('.products-carousel__slide');
+        const slideContainer = document.querySelector('.plants-pricing-carousel__slide');
         slideContainer.innerHTML = slides[currentSlide];
-        if (window.innerWidth > 600) {
-            const secondSlide = currentSlide + 1 >= slides.length ? 0 : currentSlide + 1;
-            slideContainer.innerHTML += slides[secondSlide];
-            if (window.innerWidth > 900) {
-                const thirdSlide = secondSlide + 1 >= slides.length ? 0 : secondSlide + 1;
-                slideContainer.innerHTML += slides[thirdSlide];
-            }
-        }
     }
 
     function nextSlide() {
@@ -31,10 +27,7 @@
         showCurrentSlide();
     }
 
-    // setInterval(nextSlide, 1000);
-    showCurrentSlide();
-
-    document.querySelector('button.products-carousel__button.forward').addEventListener('click', nextSlide);
-    document.querySelector('button.products-carousel__button.back').addEventListener('click', prevSlide);
+    document.querySelector('button.plants-pricing-carousel__button.forward').addEventListener('click', nextSlide);
+    document.querySelector('button.plants-pricing--carousel__button.back').addEventListener('click', prevSlide);
     window.addEventListener('resize', showCurrentSlide);
-})();
+};
