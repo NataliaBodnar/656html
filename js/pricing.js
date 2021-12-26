48b16fa7899975bb9c6bcf2b996c21d75f082f91
NataliaBodnar <bodnarnatalka@gmail.com>
2021-12-26 20:47:13 +0000

    added pricing-plants-cost-cards.js, updated css, html

--- js/pricing.js
+++ js/pricing.js
@@ -1,13 +1,13 @@
 (function() {
     const slides = [
-        '<img src="img/plants-pricing/briofillyum.svg" alt="Briofillyum">',
-        '<img src="img/plants-pricing/cedar-pine.svg" alt="Cedar Pine">',
-        '<img src="img/plants-pricing/crassula-ovata.svg" alt="Crassula Ovata">',
-        '<img src="img/plants-pricing/elephant-bush.svg" alt="Elephant Bush">'
-        '<img src="img/plants-pricing/graptosedum.svg" alt="Graptosedum">'
-        '<img src="img/plants-pricing/money-plant.svg" alt="Money Plant">'
-        '<img src="img/plants-pricing/philodendron.svg" alt="Philodendron">'
-        '<img src="img/plants-pricing/raphis-palm.svg" alt="Raphis Palm">'
+        <img src="./img/plants-pricing/briofillyum.svg" alt="Briofillyum">,
+        <img src="./img/plants-pricing/cedar-pine.svg" alt="Cedar Pine">,
+        <img src="./img/plants-pricing/crassula-ovata.svg" alt="Crassula Ovata">,
+        <img src="./img/plants-pricing/elephant-bush.svg" alt="Elephant Bush">,
+        <img src="./img/plants-pricing/graptosedum.svg" alt="Graptosedum">,
+        <img src="./img/plants-pricing/money-plant.svg" alt="Money Plant">,
+        <img src="./img/plants-pricing/philodendron.svg" alt="Philodendron">,
+        <img src="./img/plants-pricing/raphis-palm.svg" alt="Raphis Palm">
     ];
 
     let currentSlide = 0;
@@ -30,6 +30,7 @@
     showCurrentSlide();
 
     document.querySelector('button.plants-pricing-carousel-button.forward').addEventListener('click', nextSlide);
+    document.querySelector('plants-pricing-carousel__slide')
     document.querySelector('button.plants-pricing-carousel-button.back').addEventListener('click', prevSlide);
     window.addEventListener('resize', showCurrentSlide);
 };
