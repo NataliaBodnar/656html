6247beb7ffd0ed2c342314361e3883d40e51e9a8
NataliaBodnar <bodnarnatalka@gmail.com>
2021-12-26 21:02:57 +0000

    corrected errors in js

--- js/pricing.js
+++ js/pricing.js
@@ -1,19 +1,19 @@
 (function() {
     const slides = [
-        <img src="./img/plants-pricing/briofillyum.svg" alt="Briofillyum">,
-        <img src="./img/plants-pricing/cedar-pine.svg" alt="Cedar Pine">,
-        <img src="./img/plants-pricing/crassula-ovata.svg" alt="Crassula Ovata">,
-        <img src="./img/plants-pricing/elephant-bush.svg" alt="Elephant Bush">,
-        <img src="./img/plants-pricing/graptosedum.svg" alt="Graptosedum">,
-        <img src="./img/plants-pricing/money-plant.svg" alt="Money Plant">,
-        <img src="./img/plants-pricing/philodendron.svg" alt="Philodendron">,
-        <img src="./img/plants-pricing/raphis-palm.svg" alt="Raphis Palm">
+        <img src="./img/plants-pricing/briofillyum.svg" alt="Briofillyum"/>,
+        <img src="./img/plants-pricing/cedar-pine.svg" alt="Cedar Pine"/>,
+        <img src="./img/plants-pricing/crassula-ovata.svg" alt="Crassula Ovata"/>,
+        <img src="./img/plants-pricing/elephant-bush.svg" alt="Elephant Bush"/>,
+        <img src="./img/plants-pricing/graptosedum.svg" alt="Graptosedum"/>,
+        <img src="./img/plants-pricing/money-plant.svg" alt="Money Plant"/>,
+        <img src="./img/plants-pricing/philodendron.svg" alt="Philodendron"/>,
+        <img src="./img/plants-pricing/raphis-palm.svg" alt="Raphis Palm"/>
     ];
 
     let currentSlide = 0;
 
     function showCurrentSlide() {
-        const slideContainer = document.querySelector('.plants-pricing-carousel__slide');
+        const slideContainer = document.querySelector('.plants-pricing-carousel_slide');
         slideContainer.innerHTML = slides[currentSlide];
     }
 
@@ -30,7 +30,6 @@
     showCurrentSlide();
 
     document.querySelector('button.plants-pricing-carousel-button.forward').addEventListener('click', nextSlide);
-    document.querySelector('plants-pricing-carousel__slide')
     document.querySelector('button.plants-pricing-carousel-button.back').addEventListener('click', prevSlide);
     window.addEventListener('resize', showCurrentSlide);
-};
+});
